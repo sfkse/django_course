926422a0335494f076b352f4757aa2d0b5227f91
sfkse <sfkse5591@gmail.com>
2021-12-25 16:11:03 +0200

    env files

--- django_course/settings.py
+++ django_course/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gmea6=ca3go14y3l1t9n9ld#w!#98lsv*1n@ux$oh6@c!l=#gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoexercise.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
